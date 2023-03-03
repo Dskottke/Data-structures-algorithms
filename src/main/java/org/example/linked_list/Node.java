25ee503ee2e0084064b12b78db3b857c18fd71f0
davidskottke <david.skottke@neuefische.de>
2023-03-03 17:05:50 +0100

    Add Doubly linked list

--- src/main/java/org/example/linked_list/Node.java
+++ src/main/java/org/example/linked_list/Node.java
@@ -1,8 +1,8 @@
-package org.example;
+package org.example.linked_list;
 
 public class Node {
-    int value;
-    Node next;
+     int value;
+     Node next;
 
     public Node(int value , Node next) {
         this.value = value;
