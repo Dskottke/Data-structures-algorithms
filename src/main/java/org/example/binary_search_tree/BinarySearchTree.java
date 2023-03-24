0b7add11d6fe2d4f3252c95519515746ff014868
davidskottke <david.skottke@neuefische.de>
2023-03-24 09:39:16 +0100

    Refactor insert Method

--- src/main/java/org/example/binary_search_tree/BinarySearchTree.java
+++ src/main/java/org/example/binary_search_tree/BinarySearchTree.java
@@ -5,47 +5,31 @@ public class BinarySearchTree {
 
 
     public boolean insert(int value) {
+        BstNode newNode = new BstNode(value, null, null);
         if (root == null) {
-            root = new BstNode(value, null, null);
+            root = newNode;
             return true;
         }
-        BstNode find = lookUp(value);
-        if (find.value == value) {
-            return false;
-        } else if (find.value > value) {
-            find.left = new BstNode(value, null, null);
-            return true;
-        } else {
-            find.right = new BstNode(value, null, null);
-            return true;
-        }
-    }
-
-    public BstNode lookUp(int value) {
         BstNode temp = root;
-        while (temp != null) {
-            if (temp.value > value) {
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
                 if (temp.left == null) {
-                    return temp;
-                } else {
-                    temp = temp.left;
-
+                    temp.left = newNode;
+                    return true;
                 }
-
+                temp = temp.left;
             } else {
                 if (temp.right == null) {
-                    temp.right = new BstNode(value, null, null);
-                    return temp;
-                } else {
-                    temp = temp.right;
+                    temp.right = newNode;
+                    return true;
                 }
+                temp = temp.right;
             }
         }
-        return null;
     }
+
 }
-
-
 
 
 
